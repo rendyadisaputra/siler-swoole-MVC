5a0c1d604d75bb4c51b02a7665e3186637f345d6
itsupport <itsupport@cls-indo.com>
2021-02-08 11:15:00 +0700

    make setup for smtp server

--- src/Configs/configs.example.php
+++ src/Configs/configs.example.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 define('TCP_HOST', '192.168.56.66');
 define('TCP_PORT', 9502);
 
-define('MONGODB_HOST', 'mongodb://192.168.56.4:27017');
+define('MONGODB_HOST', 'mongodb://127.0.0.1:27017');
 define('JWTKEY', 'YWVybzI5MTAwMCQxMjN5');
 define('FILE_UPLOAD_DIR', __DIR__.'/../../public/images/');
 define('URL_HOST', 'http://192.168.56.66:9501/');
-
 define('SMTP_SET', '
 {
     "SMTPDebug" : 1;  
